da88061df7e04043aa9006e0ec5496f61129cb6f
Rohit Pathak <73642501+avengerrohit@users.noreply.github.com>
2021-04-12 12:41:41 +0530

    Add files via upload

---
+++ avengerrohit-install-metasploit.sh
@@ -0,0 +1,8 @@
+figlet Metasploit
+termux-setup-storage
+apt update && apt upgrade -y
+apt install git -y
+git clone https://github.com/avengerrohit/Metasploit-framework.git
+cd Metasploit-framework/
+chmod +x *
+bash metasploit.sh
